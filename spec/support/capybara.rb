0d8f7f5ebf502d1d585537b6c4897e75ca08de92
kimurasayo <otaemiiin@gmail.com>
2021-03-27 14:27:24 +0900

    Fix: capybaraの初期設定を変更する

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,7 +1,5 @@
 RSpec.configure do |config|
-  
-
-  config.before(:each, type: :system, js: true) do
-    driven_by :selenium_chrome_headless
+  config.before(:each, type: :system) do
+    driven_by :selenium, using: :headless_chrome
   end
 end
