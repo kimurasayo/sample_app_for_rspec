ac384d40bcd1dec4eea28576229365b98aefb85e
kimurasayo <otaemiiin@gmail.com>
2021-03-27 14:29:40 +0900

    Fix: テストケースを修正する

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -29,13 +29,15 @@ RSpec.describe "Users", type: :system do
       end
       context '登録済のメールアドレスを使用' do
         it 'ユーザーの新規作成が失敗する' do
+          other_user = create(:user)
           visit sign_up_path
-          fill_in 'Email', with: user.email
+          fill_in 'Email', with: other_user.email
           fill_in 'Password', with: 'password'
           fill_in 'Password confirmation', with: 'password'
           click_button 'SignUp'
           expect(current_path).to eq users_path
           expect(page).to have_content "Email has already been taken"
+          expect(page).to have_field 'Email', with: other_user.email
         end
       end
     end
@@ -43,7 +45,7 @@ RSpec.describe "Users", type: :system do
     describe 'マイページ' do
       context 'ログインしていない状態' do
         it 'マイページへのアクセスが失敗する' do
-          visit users_path
+          visit users_path(user)
           expect(current_path).to eq login_path
           expect(page).to have_content "Login required"
         end
@@ -52,10 +54,10 @@ RSpec.describe "Users", type: :system do
   end
 
   describe 'ログイン後' do
+    before { Login_as(user) }
     describe 'ユーザー編集' do
       context 'フォームの入力値が正常' do
         it 'ユーザーの編集が成功する' do
-          Login_as(user)
           visit edit_user_path(user)
           fill_in 'Email', with: 'test@example.com'
           fill_in 'Password', with: 'test'
@@ -67,18 +69,17 @@ RSpec.describe "Users", type: :system do
       end
       context 'メールアドレスが未入力' do
         it 'ユーザーの編集が失敗する' do
-          Login_as(user)
           visit edit_user_path(user)
           fill_in 'Email', with: ''
           fill_in 'Password', with: 'test'
           fill_in 'Password confirmation', with: 'test'
           click_button 'Update'
           expect(page).to have_content "Email can't be blank"
+          expect(current_path).to eq user_path(user)
         end
       end
       context '登録済のメールアドレスを使用' do
         it 'ユーザーの編集が失敗する' do
-          Login_as(user)
           user_1 = create(:user)
           visit edit_user_path(user)
           fill_in 'Email', with: user_1.email
@@ -86,14 +87,15 @@ RSpec.describe "Users", type: :system do
           fill_in 'Password confirmation', with: 'test'
           click_button 'Update'
           expect(page).to have_content "Email has already been taken"
+          expect(current_path).to eq user_path(user)
         end
       end
       context '他ユーザーの編集ページにアクセス' do
         it '編集ページへのアクセスが失敗する' do
-          Login_as(user)
           user_1 = create(:user)
           visit edit_user_path(user_1)
           expect(page).to have_content "Forbidden access."
+          expect(current_path).to eq user_path(user)
         end
       end
     end
@@ -101,13 +103,14 @@ RSpec.describe "Users", type: :system do
     describe 'マイページ' do
       context 'タスクを作成' do
         it '新規作成したタスクが表示される' do
-          Login_as(user)
-          visit new_task_path
-          fill_in 'Title', with: "title"
-          fill_in 'Content', with: 'content'
-          select 'todo', from: 'Status'
-          fill_in 'Deadline', with: '2021/3/30 17:48'
-          click_button 'Create Task'
+          create(:task, title: 'title', status: :doing, user: user)
+          visit user_path(user)
+          expect(page).to have_content('You have 1 task.')
+          expect(page).to have_content('title')
+          expect(page).to have_content('doing')
+          expect(page).to have_link('Show')
+          expect(page).to have_link('Edit')
+          expect(page).to have_link('Destroy')
         end
       end
     end
